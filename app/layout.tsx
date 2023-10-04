ac0fe9fb33cd870b52ef7e0b6d64d5afb8fd6e23
jurecapuder <capuder.j@gmail.com>
2023-10-04 21:49:41 +0200

    Updating global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-black-100 font-poppins">
+        {children}
+      </body>
     </html>
   )
 }
