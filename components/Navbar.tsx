1b0e2383314df4b309bbb4f6ce0a867781576d1e
jurecapuder <capuder.j@gmail.com>
2023-10-04 22:02:33 +0200

    Updating navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+         <Link href="/">
+          <Image
+            src="/jsm-logo.svg"
+            alt="logo"
+            width={55}
+            height={40}  
+          />
+         </Link>
+      </div>
+    </nav>
   )
 }
 
