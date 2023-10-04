b34c279681369a917f807070cbda2b4f758f8ba7
jurecapuder <capuder.j@gmail.com>
2023-10-04 21:55:05 +0200

    Importing layout components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode}) => {
   return (
     <>
-      Navbar
+      <Navbar />
       {children}
-      Footer
+      <Footer />
     </>
   )
 }
