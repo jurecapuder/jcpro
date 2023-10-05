b6859b5d896360e7f2560471cf40ef0c693e101e
jurecapuder <capuder.j@gmail.com>
2023-10-05 22:26:49 +0200

    Adding div styling and seach form

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,14 @@
+import SearchForm from "@/components/SearchForm"
 import React from 'react'
 
 const page = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <div>
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center heading2 mb-6">
           <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources</h1>
 
-
+          <SearchForm />
         </div>
       </section>
     </main>
