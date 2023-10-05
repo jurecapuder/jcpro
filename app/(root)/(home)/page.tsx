1cee36e61379ad14a26c7fababe13ac1c14ec89d
jurecapuder <capuder.j@gmail.com>
2023-10-05 22:21:04 +0200

    Updating page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Home</div>
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div>
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources</h1>
+
+
+        </div>
+      </section>
+    </main>
   )
 }
 
