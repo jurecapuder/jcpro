e4d3db7a5a92569ba257bb58ccde096c201cbfea
jurecapuder <capuder.j@gmail.com>
2023-10-05 21:45:34 +0200

    Updating footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <p>Copyright © 2023 JC Mastery Pro | All Right Reserved</p>
+
+      <div className="flex gap-x-9">
+        <Link href="/terms-of-use"> Terms & COnditions </Link>
+
+        <Link href="/privacy-policy"> Privacy Policy </Link>
+      </div>
+    </footer>
   )
 }
 
