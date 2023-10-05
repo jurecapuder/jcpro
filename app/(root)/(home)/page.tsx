c1f917b2591cb2c7e7f51af95d66213ab0da7d53
jurecapuder <capuder.j@gmail.com>
2023-10-05 22:42:00 +0200

    Fixing layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,9 +7,9 @@ const page = () => {
       <section className="nav-padding w-full">
         <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center heading2 mb-6">
           <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources</h1>
-
-          <SearchForm />
         </div>
+        
+        <SearchForm />
       </section>
     </main>
   )
