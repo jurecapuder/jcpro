f9a2991d042ef76f3682e35d2af8de4808ea0209
jurecapuder <capuder.j@gmail.com>
2023-10-06 20:15:59 +0200

    Adding ul styling

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -6,7 +6,7 @@ const links = ["all", "Next 13", "Frontend", "Backend", "Fullstack"]
 
 const Filters = () => {
   return (
-    <ul>
+    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
       {links.map((link) => (
         <button
           key={link}
