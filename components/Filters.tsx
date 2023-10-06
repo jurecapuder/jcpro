39e5f7e1ee37ee03e6cb57a24feb6f12b752da69
jurecapuder <capuder.j@gmail.com>
2023-10-06 20:26:41 +0200

    Creating handle filter

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -5,14 +5,18 @@ import React, { useState } from 'react'
 const links = ["all", "Next 13", "Frontend", "Backend", "Fullstack"]
 
 const Filters = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+
+  const handleFilter = (link: string) => {
+    setActive(link);
+  }
 
   return (
     <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
       {links.map((link) => (
         <button
           key={link}
-          onClick={() => {}}
+          onClick={() => {handleFilter(link)}}
           className="whitespace-nowrap rounded-lg px-8 py-2.5 capitalize"
         >
           {link}
