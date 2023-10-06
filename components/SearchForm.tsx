4708c506007e2b0aaaef384ef7b23ba6ead779b4
jurecapuder <capuder.j@gmail.com>
2023-10-06 19:52:18 +0200

    Updating search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,8 +1,26 @@
+import Image from "next/image"
 import React from 'react'
+import { Input } from "@/components/ui/input"
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
+      <label className="flex-center relative w-full max-w-3xl">
+        <Image
+          src="/magnifying-glass.svg"
+          className="absolute left-8"
+          width={32}
+          height={32}
+          alt="Search Icon"
+        />
+
+        <Input
+          className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
+          type="text"
+          placeholder="Search"
+        />
+      </label>
+    </form>
   )
 }
 
