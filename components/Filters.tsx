433a9a16c72e94371867815e9d4b3b14a0a40a9b
jurecapuder <capuder.j@gmail.com>
2023-10-06 20:07:39 +0200

    Updating filters component

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,8 +1,21 @@
+"use client";
+
 import React from 'react'
+
+const links = ["all", "Next 13", "Frontend", "Backend", "Fullstack"]
 
 const Filters = () => {
   return (
-    <div>Filters</div>
+    <ul>
+      {links.map((link) => (
+        <button
+          key={link}
+          onClick={() => {}}
+        >
+
+        </button>
+      ))}
+    </ul>
   )
 }
 
