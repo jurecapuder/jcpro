e3ff87d0177313b36b8404f96f6a9647c66c8451
jurecapuder <capuder.j@gmail.com>
2023-10-06 22:03:50 +0200

    Updating sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,14 +8,14 @@ import {deskTool} from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemas/resource.schema'
+import schemas from './sanity/schemas'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
-  schema,
+  schema: { types : schemas },
   plugins: [
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
