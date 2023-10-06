9ac086eebf07177b9641158946c44af86111c57f
jurecapuder <capuder.j@gmail.com>
2023-10-06 20:18:50 +0200

    Importing use state

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const links = ["all", "Next 13", "Frontend", "Backend", "Fullstack"]
 
 const Filters = () => {
+  const [active, setActive] = useState("")
+
   return (
     <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
       {links.map((link) => (
