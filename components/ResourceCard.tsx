222a43dc9447789c0e08b9225b55dd8351e6f04a
jurecapuder <capuder.j@gmail.com>
2023-10-07 19:34:38 +0200

    Creating resource card

---
+++ components/ResourceCard.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  id: string;
+  title: string;
+  image: string;
+  downloadNumber: number;
+  slug: string;
+}
+
+const ResourceCard = ({ id, title, image, downloadNumber, slug }: Props) => {
+  return (
+    <div>ResourceCard</div>
+  )
+}
+
+export default ResourceCard
