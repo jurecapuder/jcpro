3e7384f79ef50eb0652433b5cbbf660293df0e65
jurecapuder <capuder.j@gmail.com>
2023-10-07 19:25:26 +0200

    Updating get resources sanity action

--- sanity/actions.ts
+++ sanity/actions.ts
@@ -1,5 +1,6 @@
 import { groq } from 'next-sanity';
 import { readClient } from "./lib/client";
+import { buildQuery } from "./utils";
 
 interface GetResourcesParams {
   query: string;
@@ -12,9 +13,24 @@ export const getResources = async (params: GetResourcesParams) => {
 
   try {
     const resources = await readClient.fetch(
-      groq``
-    )
+      groq`${buildQuery({
+        type: "resource",
+        query,
+        category,
+        page: parseInt(page),
+      })}{
+        title,
+        _id,
+        downloadLink,
+        "image": poster.asset->url,
+        views,
+        slug,
+        category
+      }`
+    );
+
+    return resources;
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
