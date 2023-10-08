f49e1aff4275e85a878cec4d3e59af82a1c7ccce
jurecapuder <capuder.j@gmail.com>
2023-10-08 20:11:15 +0200

    Updating navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 bg-black-100 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
-         <Link href="/">
+        <Link href="/">
           <Image
             src="/jsm-logo.svg"
-            alt="logo"
             width={55}
-            height={40}  
+            height={40}
+            alt="logo"
           />
-         </Link>
+        </Link>
 
         <Image
           src="/hamburger-menu.svg"
@@ -26,16 +25,16 @@ const Navbar = () => {
         <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
           <li className="body-text text-gradient_blue-purple !font-bold">
             <Link
-              href="https://www.jsmastery.pro/ultimate-next13-course"
+              href="https://jsmastery.pro/next13"
               target="_blank"
             >
               Next.js 13.4 Course
             </Link>
           </li>
 
-          <li className="body-text !body-normal">
+          <li className="body-text !font-normal">
             <Link
-              href="https://www.jsmastery.pro/masterclass"
+              href="https://jsmastery.pro/masterclass"
               target="_blank"
             >
               Masterclass
