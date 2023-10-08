d719929d4f3bf2480c0c2dd070d6b7746ce7cc18
jurecapuder <capuder.j@gmail.com>
2023-10-08 13:50:25 +0200

    Updating resource card

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
   id: string;
   title: string;
@@ -8,7 +19,33 @@ interface Props {
 
 const ResourceCard = ({ id, title, image, downloadNumber, slug }: Props) => {
   return (
-    <div>ResourceCard</div>
+    <Card className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]">
+      <Link href={`/resource/${id}`}>
+        <CardHeader>
+          <div>
+            <Image
+              src={image}
+              className="h-full rounded-md object-cover"
+              width={384}
+              height={440}
+              alt={title}
+            />
+          </div>
+
+          <CardTitle>Card Title</CardTitle>
+
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+      </Link>
+
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
   )
 }
 
