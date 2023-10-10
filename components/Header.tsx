60382206ede6ec9ac1d63e2cc7b801d6bff4d278
jurecapuder <capuder.j@gmail.com>
2023-10-10 16:06:26 +0200

    Updating header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+interface Props {
+  query: string
+  category: string
+}
 
-const Header = () => {
+const Header = ({ query, category }: Props) => {
+  if(query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for "{query}" in
+        <span className="capitalize"> {category}</span>
+      </h1>
+    )
+  }
+
   return (
-    <div>Header</div>
+    <h1 className="heading3 self-start text-white-800">
+      No results
+    </h1>
   )
 }
 
