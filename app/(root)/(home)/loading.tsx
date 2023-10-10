ee8c5ef5fdf5aa2c1b47dcfb2a9a1324c1aecb32
jurecapuder <capuder.j@gmail.com>
2023-10-10 17:50:04 +0200

    Updating loading page

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
 
 const loading = () => {
   return (
-    <div>loading</div>
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <Skeleton className="w-[100px] h-[20px] rounded-full" />
+      </section>
+    </main>
   )
 }
 
