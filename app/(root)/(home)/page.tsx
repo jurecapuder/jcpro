2ed987043ef331364e9059193b4ced49980acd45
jurecapuder <capuder.j@gmail.com>
2023-10-10 17:46:41 +0200

    Updating page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -48,6 +48,7 @@ const Page = async ({ searchParams }: Props) => {
                   id={resource._id}
                   image={resource.image}
                   downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
                 />
               ))
             ): (
@@ -64,13 +65,14 @@ const Page = async ({ searchParams }: Props) => {
           <h1 className="heading3 self-start text-white-800">{item.title}</h1>
 
           <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-            {resources.map((resource: any) => (
+            {item.resources.map((resource: any) => (
               <ResourceCard
                 key={resource._id}
                 title={resource.title}
                 id={resource._id}
                 image={resource.image}
                 downloadNumber={resource.views}
+                downloadLink={resource.downloadLink}
               />
             ))}
           </div>
