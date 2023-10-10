a0898c2df1f437a88374865d5a01edbfed6c21d7
jurecapuder <capuder.j@gmail.com>
2023-10-10 14:29:44 +0200

    Updating search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,11 +1,38 @@
 "use client";
 
 import Image from "next/image"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Input } from "@/components/ui/input"
+import { formUrlQuery } from "@/sanity/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const SearchForm = () => {
+  const searchParams = useSearchParams();
+
+  const router = useRouter();
+
+  const pathname = usePathname();
+
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if(search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search
+        })
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+  
+    return () => clearTimeout(delayDebounceFn)
+  }, [search])
+  
 
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
