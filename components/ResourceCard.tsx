f5dd0332faeb8f33311771373091686d08a6294c
jurecapuder <capuder.j@gmail.com>
2023-10-10 17:46:34 +0200

    Adding download link to resource card

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -12,12 +12,13 @@ interface Props {
   title: string;
   image: string;
   downloadNumber: number;
+  downloadLink: string;
 }
 
-const ResourceCard = ({ id, title, image, downloadNumber }: Props) => {
+const ResourceCard = ({ id, title, image, downloadNumber, downloadLink }: Props) => {
   return (
     <Card className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]">
-      <Link href={`/resource/${id}`}>
+      <Link href={`${downloadLink}`} target="_blank">
         <CardHeader className="flex-center flex-col gap-2.5 !p-0">
           <div className="h-fit w-full">
             <Image
@@ -44,7 +45,7 @@ const ResourceCard = ({ id, title, image, downloadNumber }: Props) => {
           {downloadNumber}
         </div>
 
-        <Link href={`/resource/${id}`} className="flex-center text-gradient_purple-blue body-semibold gap-1.5">
+        <Link href={`${downloadLink}`} target="_blank" className="flex-center text-gradient_purple-blue body-semibold gap-1.5">
           Download Now
 
           <Image
