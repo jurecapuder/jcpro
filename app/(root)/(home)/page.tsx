0d25bae04cddfcdda81d20465ad5b259d5a3dbcd
jurecapuder <capuder.j@gmail.com>
2023-10-10 17:09:13 +0200

    Updating page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -30,30 +30,32 @@ const Page = async ({ searchParams }: Props) => {
 
       <Filters />
 
-      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        <Header
-          query={searchParams?.query || ""}
-          category={searchParams?.category || ""}
-        />
+      {(searchParams?.query || searchParams?.category) && (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
 
-        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          {resources?.length > 0 ? (
-            resources.map((resource: any) => (
-              <ResourceCard
-                key={resource._id}
-                title={resource.title}
-                id={resource._id}
-                image={resource.image}
-                downloadNumber={resource.views}
-              />
-            ))
-          ): (
-            <p className="body-regular text-white-400">
-              No resources found
-            </p>
-          )}
-        </div>
-      </section>
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                />
+              ))
+            ): (
+              <p className="body-regular text-white-400">
+                No resources found
+              </p>
+            )}
+          </div>
+        </section>
+      )}
     </main>
   )
 }
