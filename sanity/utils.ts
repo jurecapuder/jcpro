85ed079830701b3c0c6c6b393baff75a1c9d83a2
jurecapuder <capuder.j@gmail.com>
2023-10-10 13:58:49 +0200

    Updating utils

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -42,8 +42,7 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
   const currentUrl = qs.parse(params);
 
   currentUrl[key] = value;
-
-  return qs.stringify(
+  return qs.stringifyUrl(
     { url: window.location.pathname, query: currentUrl },
     { skipNull: true }
   )
