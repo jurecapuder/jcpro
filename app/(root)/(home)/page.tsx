26aeac69b05b2953df312f47cd774fe4b52d2901
jurecapuder <capuder.j@gmail.com>
2023-10-10 17:38:02 +0200

    Implementing get resources playlist to page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Filters from "@/components/Filters"
 import Header from "@/components/Header"
 import ResourceCard from "@/components/ResourceCard"
 import SearchForm from "@/components/SearchForm"
-import { getResources } from "@/sanity/actions"
+import { getResources, getResourcesPlaylist } from "@/sanity/actions"
 import React from 'react'
 
 export const revalidate = 900;
@@ -17,6 +17,8 @@ const Page = async ({ searchParams }: Props) => {
     category: searchParams?.category || "",
     page: "1"
   })
+
+  const resourcesPlaylist = await getResourcesPlaylist();
 
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
@@ -56,6 +58,24 @@ const Page = async ({ searchParams }: Props) => {
           </div>
         </section>
       )}
+
+      {resourcesPlaylist.map((item: any) => (
+        <section key={item._id} className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <h1 className="heading3 self-start text-white-800">{item.title}</h1>
+
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources.map((resource: any) => (
+              <ResourceCard
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                downloadNumber={resource.views}
+              />
+            ))}
+          </div>
+        </section>
+      ))}
     </main>
   )
 }
