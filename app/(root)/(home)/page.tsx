01db063730f48bb27f458f3f33726b4c7d199225
jurecapuder <capuder.j@gmail.com>
2023-10-11 19:57:42 +0200

    Updating page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,8 @@
-import Filters from "@/components/Filters"
-import Header from "@/components/Header"
-import ResourceCard from "@/components/ResourceCard"
-import SearchForm from "@/components/SearchForm"
-import { getResources, getResourcesPlaylist } from "@/sanity/actions"
-import React from 'react'
+import Filters from "@/components/Filters";
+import Header from "@/components/Header";
+import ResourceCard from "@/components/ResourceCard";
+import SearchForm from "@/components/SearchForm";
+import { getResources, getResourcesPlaylist } from "@/sanity/actions";
 
 export const revalidate = 900;
 
@@ -23,7 +22,7 @@ const Page = async ({ searchParams }: Props) => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center heading2 mb-6">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
           <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources</h1>
         </div>
 
